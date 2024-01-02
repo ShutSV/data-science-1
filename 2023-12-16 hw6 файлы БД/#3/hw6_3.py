cfc24f3e0668482d90f1bac686591e243acafa09
ShutSV <132217445+ShutSV@users.noreply.github.com>
2024-01-02 11:50:15 +0300

    hw6 #3upd

--- 2023-12-16 hw6 файлы БД/#3/hw6_3.py
+++ 2023-12-16 hw6 файлы БД/#3/hw6_3.py
@@ -1,23 +1,20 @@
 from psycopg2 import connect as connect
-# from hw6_2 import query_get, query_set, query_n_print
-# from time import time
-# на примере БД dvd
+from datetime import datetime
+import numpy as np
+
+# на примере БД dvdrental
 
 
 # проверкв подключения к БД
 def _get_db_session(dbname, user, password):
     try:
         conn = connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host='0.0.0.0',
-            port=5432
+            dbname=dbname, user=user, password=password, host="0.0.0.0", port=5432
         )
         print("\nСоединение с БД установлено", conn, "\n")
         return conn
     except Exception as e:
-        print('БД не доступна. Проверьте Docker', e)
+        print("БД не доступна. Проверьте Docker", e)
         return False
 
 
@@ -37,14 +34,24 @@ def query_n_print(conn, query_read: str):
         print(row)
 
 
-dbname='dvdrental'
-user='admin'
-password='admin'
+# функция выполнения запроса к БД не предполагающая получения ответа
+def query_set(conn, query: str):
+    with conn.cursor() as curs:
+        curs.execute(query)
+        conn.commit()
+        print(f"выполнен запрос к БД: '{query}' \n")
+
+
+dbname = "dvdrental"
+user = "admin"
+password = "admin"
 conn = _get_db_session(dbname, user, password)
 
 # простые запросы типа query = """select * from category"""
 query_n_print(conn, """select * from category limit 20""")
 query_n_print(conn, """select * from film_category limit 20""")
+query_n_print(conn, """select * from customer where first_name = 'John'""")
+
 
 # SQL-запрос с использованием подзапросов (subqueries) для выбора данных из нескольких таблиц.
 
@@ -120,7 +127,7 @@ WHERE c.category_id is NULL OR f.category_id is NULL
 """
 query_n_print(conn, query)
 
-# Объединение ТРЕХ таблиц по полю "film_id" (которое есть во всех таблицах),
+# Объединение ТРЕХ таблиц по полю "film_id" (поле есть во всех таблицах)
 # с выводом только тех строк, которые совпадают во всех таблицах
 query = """SELECT
     i.film_id id_film_inventory,
@@ -136,6 +143,43 @@ query_n_print(conn, query)
 
 
 # Создание индекса на одной из таблиц и измерение производительности запроса до и после создания индекса.
+# --- измерять производительность лучше поочередно до и после создания индекса во избежания влияния кэширования
+# Сперва проверка времени запроса c индексом, Добавление индекса на столбец "length"
+# затем - после удаления индекса
+
+# пакетная проверка времени запросов, перемежаемых иными
+query_set(conn, """CREATE INDEX IF NOT EXISTS index_length ON film (length)""")
+time_with_index_array = np.array([])
+
+for i in range(100):
+    for j in range(100):
+        start_time = datetime.now()
+        query_get(conn, f"""SELECT * FROM film WHERE length = {80 + j}""")
+        stop_time = datetime.now()
+        time_with_index_array = np.append(
+            time_with_index_array,
+            [stop_time - start_time],
+        )
+        query_get(conn, f"""SELECT * FROM film LIMIT {1 + j}""")
+
+query_set(conn, """DROP INDEX IF EXISTS index_length""")
+time_witout_index_array = np.array([])
+
+for i in range(100):
+    for j in range(100):
+        start_time = datetime.now()
+        query_get(conn, f"""SELECT * FROM film WHERE length = {80 + j}""")
+        stop_time = datetime.now()
+        time_witout_index_array = np.append(
+            time_witout_index_array,
+            [stop_time - start_time],
+        )
+        query_get(conn, f"""SELECT * FROM film LIMIT {1 + j}""")
+
+print(
+    f"\nСреднее время {len(time_witout_index_array)} запросов без индекса = {time_witout_index_array.mean()}",
+    f"\nСреднее время {len(time_with_index_array)} запросов c индексом = {time_with_index_array.mean()}",
+)
 
 
 # SQL-запрос с использованием оконных функций (window functions) для анализа данных внутри окна.
